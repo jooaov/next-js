cf75ba4ea97d6f79874bcafc886b2a3aadddc803
jooaov <jjoao.jv13@gmail.com>
2021-03-20 17:21:32 -0300

    paginaçao

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 function Home(){
     return (
     <div>
-        <h1>Home dev</h1>
-        <Link href="/sobre">
-            <a>Pagina sobre</a>
+        <h1>sobre</h1>
+        <Link href="/">
+            <a>Pagina home</a>
         </Link>
     </div>)
 }
